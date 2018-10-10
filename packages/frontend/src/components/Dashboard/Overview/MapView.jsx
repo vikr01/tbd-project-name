b814a43a8ffbae69e69d1d9202b7e1737bc8ce45
thomaspedersen <thoepedersen@gmail.com>
2018-10-09 22:28:25 -0700

    added basic Maps view to dashboard

--- packages/frontend/src/components/Dashboard/Overview/MapView.jsx
+++ packages/frontend/src/components/Dashboard/Overview/MapView.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+// @flow
+import React, { Fragment } from 'react';
 
 import { Typography } from '@material-ui/core';
+import GoogleMapReact from 'google-map-react';
 
 const MapView = () => (
-  <Typography variant="display1" className="dashboardComponent">
-    MapView
-  </Typography>
+  <Fragment>
+    <Typography variant="display1" className="dashboardComponent2">
+      Map
+    </Typography>
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        // bootstrapURLKeys={API_KEY}
+        defaultCenter={{ lat: 37.4151628, lng: -122.000171 }}
+        defaultZoom={11}
+      />
+    </div>
+  </Fragment>
 );
 
 export default MapView;
