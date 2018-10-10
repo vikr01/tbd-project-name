f89bd1bf6d48ba05560ca3da93a5182c4efa4936
thomaspedersen <thoepedersen@gmail.com>
2018-10-09 22:28:25 -0700

    added basic Maps view to dashboard

--- packages/frontend/src/components/Dashboard/Overview/index.jsx
+++ packages/frontend/src/components/Dashboard/Overview/index.jsx
@@ -6,12 +6,8 @@ import MapView from './MapView';
 const OverviewView = () => (
   <Fragment>
     <Paper className="paperDashboard">
-      <Typography variant="display1" gutterBottom component="h2">
-        OverviewView
-      </Typography>
       <MapView />
     </Paper>
-    <div className="rest" />
   </Fragment>
 );
 
