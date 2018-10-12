6142f7eae0bd076b427b219ef2ef602f9f8e9ed6
vikr01 <vik120@icloud.com>
2018-10-12 00:46:01 -0700

    use babelrcRoots in babel.config.js to avoid combined babel config

---
+++ packages/frontend/.babelrc.js
@@ -0,0 +1,43 @@
+const devEnvs = ['development', 'test'];
+
+const devPlugins = [];
+
+const prodPlugins = [
+  [
+    require('babel-plugin-transform-react-remove-prop-types'),
+    { removeImport: true },
+  ],
+  require('@babel/plugin-transform-react-constant-elements'),
+  require('@babel/plugin-transform-react-inline-elements'),
+];
+
+module.exports = api => {
+  const isDevEnv = api.env(devEnvs);
+  const isTestEnv = api.env('test');
+
+  return {
+    presets: [
+      [
+        require('@babel/preset-env'),
+        Object.assign(
+          { useBuiltIns: 'usage' },
+          isTestEnv ? { targets: { node: 10 } } : {}
+        ),
+      ],
+      require('@babel/preset-flow'),
+      [require('@babel/preset-react'), { development: isDevEnv }],
+    ],
+
+    plugins: [
+      ...(isDevEnv ? devPlugins : prodPlugins),
+      isTestEnv
+        ? require('babel-plugin-dynamic-import-node')
+        : require('@babel/plugin-syntax-dynamic-import'),
+      [require('@babel/plugin-proposal-class-properties'), { loose: true }],
+      [
+        require('@babel/plugin-transform-runtime'),
+        { helpers: false, regenerator: true },
+      ],
+    ],
+  };
+};
