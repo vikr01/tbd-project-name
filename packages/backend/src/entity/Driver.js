047188a9aa71e8d752e75783feccdcfa0c5a58b8
Suraj_Setty <surajsetty97@gmail.com>
2018-10-16 22:48:15 -0700

    Added drivers schema

---
+++ packages/backend/src/entity/Driver.js
@@ -0,0 +1,24 @@
+// @flow
+
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+import { EntityBase } from '../entityBase';
+
+@Entity()
+export class Driver /* extends EntityBase */ {
+  @PrimaryColumn('varchar')
+  username = '';
+
+  @Column('float')
+  currentLatitude = 0;
+
+  @Column('float')
+  currentLongitude = 0;
+
+  @Column('float')
+  rating = 0;
+
+  @Column('int')
+  totalReviews = 0;
+}
+
+export default Driver;
